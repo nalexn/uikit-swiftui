fff2b8928ed250fb37545be93462d3bc1564bf7e
Alexey Naumov <alexey@naumov.tech>
2020-09-12 18:55:31 +0300

    Initial commit

---
+++ UIKitToSwiftUI/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  UIKitToSwiftUI
+//
+//  Created by Alexey Naumov on 12.09.2020.
+//  Copyright © 2020 Alexey Naumov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        window.rootViewController = RootViewController()
+        window.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ UIKitToSwiftUI/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UIKitToSwiftUI
+//
+//  Created by Alexey Naumov on 12.09.2020.
+//  Copyright © 2020 Alexey Naumov. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
